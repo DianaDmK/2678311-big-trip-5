5ccd28d8ae1e3c886e3da8939d826ff15bea09f6
DianaDmK <kalyakina.130802@mail.ru>
2026-01-25 02:22:35 +0700

    feat: 1.25. Time to break the stones

---
+++ src/presenter/main-presenter.js
@@ -0,0 +1,26 @@
+import TripInfoView from '../view/trip-info-view.js';
+// import CreateFormView from '../view/create-form-view.js';
+import EditFormView from '../view/edit-form-view.js';
+import FiltersView from '../view/filters-view.js';
+import RoutePointView from '../view/route-point-view.js';
+import SortView from '../view/sort-view.js';
+import { render, RenderPosition } from '../render.js';
+
+export default class MainPresenter {
+  constructor() {
+    this.tripMainContainer = document.querySelector('.trip-main');
+    this.filtersContainer = document.querySelector('.trip-controls__filters');
+    this.tripEventsContainer = document.querySelector('.trip-events');
+  }
+
+  init() {
+    render(new TripInfoView(), this.tripMainContainer, RenderPosition.AFTERBEGIN);
+    render(new FiltersView(), this.filtersContainer);
+    render(new EditFormView(), this.tripEventsContainer, RenderPosition.AFTERBEGIN);
+    render(new SortView(), this.tripEventsContainer, RenderPosition.AFTERBEGIN);
+
+    for (let i = 0; i < 3; i++) {
+      render(new RoutePointView(), this.tripEventsContainer);
+    }
+  }
+}
