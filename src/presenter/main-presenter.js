cb07f391f0075bc135e4fab9112e3e3ac2a61a05
DianaDmK <kalyakina.130802@mail.ru>
2026-01-25 20:47:18 +0700

    chore: creature point-list-view.js

--- src/presenter/main-presenter.js
+++ src/presenter/main-presenter.js
@@ -4,7 +4,9 @@ import EditFormView from '../view/edit-form-view.js';
 import FiltersView from '../view/filters-view.js';
 import RoutePointView from '../view/route-point-view.js';
 import SortView from '../view/sort-view.js';
+import PointListView from '../view/point-list-view.js';
 import { render, RenderPosition } from '../render.js';
+
 
 export default class MainPresenter {
   constructor() {
@@ -14,13 +16,16 @@ export default class MainPresenter {
   }
 
   init() {
+    const pointListView = new PointListView();
+
     render(new TripInfoView(), this.tripMainContainer, RenderPosition.AFTERBEGIN);
     render(new FiltersView(), this.filtersContainer);
-    render(new EditFormView(), this.tripEventsContainer, RenderPosition.AFTERBEGIN);
     render(new SortView(), this.tripEventsContainer, RenderPosition.AFTERBEGIN);
+    render(pointListView, this.tripEventsContainer);
+    render(new EditFormView(), pointListView.getElement());
 
     for (let i = 0; i < 3; i++) {
-      render(new RoutePointView(), this.tripEventsContainer);
+      render(new RoutePointView(), pointListView.getElement());
     }
   }
 }
